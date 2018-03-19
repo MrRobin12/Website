ecfba74d638033e7d0408157a09a4cafc5e40e4c
Robin Johannesson <robinspelaspel@gmail.com>
2018-03-19 18:15:45 +0100

    New Update To Mobile!
    - Template to mobile add buttons!
    - Snapchat, Instagram and Twitter add button!

---
+++ tabs/scripts/mobile_overlay.js
@@ -0,0 +1,7 @@
+function mobileOn() {
+    document.getElementById("mobile-overlay").style.display = "block";
+}
+
+function mobileOff() {
+    document.getElementById("mobile-overlay").style.display = "none";
+}
---
+++ tabs/scripts/tablet_overlay.js
@@ -0,0 +1,7 @@
+function tabletOn() {
+    document.getElementById("tablet-overlay").style.display = "block";
+}
+
+function tabletOff() {
+    document.getElementById("tablet-overlay").style.display = "none";
+}
