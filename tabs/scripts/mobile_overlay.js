d83af7210da6d57e174f136c98c615b0772883f0
Robin Johannesson <robinspelaspel@gmail.com>
2018-03-25 19:47:04 +0200

    New Update!
    Mobile new gui, menu bar!

--- tabs/scripts/mobile_overlay.js
+++
@@ -1,7 +0,0 @@
-function mobileOn() {
-    document.getElementById("mobile-overlay").style.display = "block";
-}
-
-function mobileOff() {
-    document.getElementById("mobile-overlay").style.display = "none";
-}
