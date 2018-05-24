127c3fdac8aa4b28fc344acfaaf0b8a41b1da66a
Robin Johannesson <34487502+MrRobin12@users.noreply.github.com>
2018-05-24 13:34:20 +0200

    Add files via upload

---
+++ tabs/universal/google_search.js
@@ -0,0 +1,12 @@
+if(screen.width >= 769)
+{
+document.getElementById('banner-desktop').alt = "MrRobin12 Banner Desktop";   
+}   
+else if(screen.width >= 426)
+{
+
+} 
+else
+{
+document.getElementById('banner-mobile').alt = "MrRobin12 Banner Mobile";   
+} 
