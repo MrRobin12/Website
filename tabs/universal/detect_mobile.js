a6c81d751bd7264bd341b825d8024f86a2b863dd
Robin Johannesson <34487502+MrRobin12@users.noreply.github.com>
2018-05-24 12:01:04 +0200

    Update detect_mobile.js

--- tabs/universal/detect_mobile.js
+++ tabs/universal/detect_mobile.js
@@ -1,15 +1,1 @@
-if(screen.width >= 769)
-{
-var win = window.open("tabs/desktop/home.html", '_self');
-win.focus();    
-}   
-else if(screen.width >= 426)
-{
-var win = window.open("tabs/tablet/home.html", '_self');
-win.focus();    
-} 
-else
-{
-var win = window.open("tabs/mobile/home.html", '_self');
-win.focus();    
-} 
+
