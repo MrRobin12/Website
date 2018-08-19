80a751544cfc95b681544afd21e4b9793a832648
Robin Johannesson <robinspelaspel@gmail.com>
2018-08-19 21:33:36 +0200

    Update!

--- tabs/music_player/song_description.js
+++ tabs/music_player/song_description.js
@@ -9,8 +9,8 @@ jQuery.get('https://cors-anywhere.herokuapp.com/https://dl.dropbox.com/s/ht9gi89
     var i = 0;
     
     for (i = 0; i < count; i++) {        
-        var song_title = "Title: " + data[i].Song_title;
-        var artist = "Artist: " + data[i].Artist;
+        var song_title = "<font class='title'>" + data[i].Song_title + "</font>";
+        var artist = "<font class='artist'>" + data[i].Artist + "</font>";
         
         function formatTime(seconds) {
             minutes = Math.floor(seconds / 60);
@@ -21,21 +21,31 @@ jQuery.get('https://cors-anywhere.herokuapp.com/https://dl.dropbox.com/s/ht9gi89
         }    
         
         var ms = data[i].duration_ms / 1000;
-        var duration =  formatTime(ms);
+        var duration =  "<font class='duration'>" + "Duration: " + formatTime(ms) + "</font>";
+        
+        var explicit = "<br>";
+        
+        if (data[i].explicit == true) {
+            explicit =  "<br><br>" + "<font class='explicit'>" + "EXPLICIT" + "</font>" + "<br>";
+        }
+        
+        var date = "<font class='date'>" + "Created: " + data[i].album.release_date + "</font>";
+        var album = "<font class='album'>" + "Album: " + data[i].album.name + " : " + data[i].album.album_type + "</font>";
         
         var audio_url = "https://cors-anywhere.herokuapp.com/https://dl.dropbox.com/s/bh06jvg1y933npv/Rihanna%20-%20We%20Found%20Love%20Bootleg%20Remix%20%28MrRobin%29.mp3";
         
         var song_url = "<div id='songURL" + i + "' src='" +  data[i].Song_url + "'></div>";     
         var image_cover = data[i].album.images[2].url;
-        var img = "<img src='" + image_cover + "' style='width: 32px; height: 32px; margin-right: 10px;'>";
+        var img = "<img class='image_cover' src='" + image_cover + "' style=''>";
         var border = "<div id='border" + i + "' onclick='activate(" + i + ")' style='border: 2px solid black; padding-left: 10px; padding-right: 10px; padding-top: 10px; margin-bottom: 25px; width: 280px;'>"
 
-        var result = song_url + border + img + song_title + "<br>" + artist + "<br><br>" + duration + "<br><br>" + "<br>" + "</div>";     
+        var result = song_url + border + img + artist + "<br><br>" + song_title + "<br><br>" + album + "<br>" + duration + explicit + "<br>" + date + "<br><br>" + "</div>";     
         
         document.getElementById("song_api").innerHTML += result;
     }
     
     localStorage.setItem("object_length", i);
+    document.getElementById("border1").style.borderColor = "rgb(154, 4, 219)"; 
 });
 
 object_length = localStorage.getItem("object_length");
